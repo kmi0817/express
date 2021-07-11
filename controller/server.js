eca2d3923f8d01f6c3762e68a986054d0d4f4c17
kmi0817 <kmi0817@naver.com>
2021-07-11 19:45:55 +0900

    template 모듈화, XMLHttpRequest 사용해 데이터 인-메모리화

--- controller/server.js
+++ controller/server.js
@@ -1,145 +1,19 @@
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-import XMLHttpRequest from 'xhr2';
+import { HTML, INDEX, MOVIE_DISPLAY, EDIT, EDIT_FUNC } from '../lib/template.js';
 
+/* 클라이언트 상에서 사용할 데이터 이름: myData */
 
-// let data = []
-
-
-function HTML(body, func) {
-    return `
-    <!doctype HTML>
-    <html>
-        <head>
-            <meta charset='utf-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>TEST</title>
-            <link rel="stylesheet" href="http://cdn.webix.com/edge/webix.css" type="text/css">
-            <script src="//cdn.webix.com/edge/webix.js" type="text/javascript"></script>
-        
-            <style>
-                a {text-decoration: none; color: #1CA1C1}
-            </style>
-        </head>
-        <body>
-        <script type='text/javascript' charset='utf-8'>
-            webix.ready(function() {
-                webix.ui({
-                    type: 'space',
-                    rows: [
-                        {
-                            cols: [
-                                {
-                                    view: 'button',
-                                    value: 'index',
-                                    click: function() {
-                                        location.href='/';
-                                    }
-                                },
-                                {template: 'searchBox'},
-                                {template: 'menu1'},
-                                {template: 'menu2'},
-                                {template: 'menu3'},
-                                {template: 'menu4'},
-                            ],
-                            height: 60
-                        },
-                        {
-                            ${body}
-                        },
-                        {
-                            template: 'footer', height: 50
-                        }
-                    ]
-                });
-                ${func}
-            });
-        </script>
-        </body>
-    </html>
-    `;
+export const getIndex = (req, res) => {
+    const body = INDEX();
+    const template = HTML(body, '');
+    res.send(template);
 }
 
-function LIST(movielist) {
-    // movielist 를 JSON 형태로 바꾼다. ex) {id: "1233..."}
-    let movie_list = []
-    for (var i = 0; i <movielist.length; i++)
-        movie_list.push({id: movielist[i]});
-    
-    movie_list = JSON.stringify(movie_list); // 이게 없으면 [object Object]로 인식
-    
-    const list = `
-    view: 'list', id: 'list', select: true, data: ${movie_list},
-    template: "<a href='movies/#id#'>#id#</a>"
-    `;
+export const getMovies = (req, res) => {
+    /* localhost:3000/movies/에 게시할 JSON 데이터
+        이 주소로 XMLHttpRequest 객체가 GET 해서 데이터를 사용한다 */
 
-    return list;
-}
-
-function MOVIE_DISPLAY(id, title, director, release_date, description) {
-    const table = `
-    rows: [
-        {
-            height: 35,
-            cols: [
-                {template: "제목"},
-                {template: "${title}"}
-            ]
-        },
-        {
-            height: 35,
-            cols: [
-                {template: "감독"},
-                {template: "${director}"},
-                {template: "개봉년도"},
-                {template: "${release_date}"}
-            ]
-        },
-        {
-            template: "${description}", minHeight: 200, autoheight: true
-        },
-        {
-            view: 'toolbar',
-            elements: [
-                {
-                    view: 'button',
-                    value: '목록',
-                    autowidth: true,
-                    click: function() {
-                        location.href='/';
-                    }
-                },
-                {
-                    view: 'button',
-                    value: '수정',
-                    autowidth: true,
-                    click: function() {
-                        location.href='/edit/${id}';
-                    }
-                },
-                {
-                    view: 'button',
-                    value: '삭제',
-                    autowidth: true,
-                    click: function() {
-                        webix.confirm("삭제하시겠습니까?", "confirm-warning")
-                        .then((result) => {
-                            webix.ajax().post('/delete_process', {id: "${id}"})
-                            .then((result) => { location.href='/'; })
-                            .fail(() => { webix.message('삭제하는 데 문제가 발생했습니다.')});
-                        });
-                    }
-                }
-            ]
-        }
-    ]
-    `;
-
-    /* confirm documentation: https://docs.webix.com/api___confirm.html */
-    return table
-}
-
-function getData() {
     let data = []
     
     const movielist = fs.readdirSync('movies');
@@ -151,57 +25,7 @@ function getData() {
         // console.log(element);
         data.push(element);
     });
-    return data;
-}
 
-function TEMP() {
-    return `
-    view: 'datatable',
-    select: true,
-    id: 'table',
-    url: 'http://localhost:3000/movies',
-    columns: [
-        {id: 'title', header: '제목', adjust: true},
-        {id: 'director', header: '감독', adjust: true},
-        {id: 'release_date', header: '개봉년도', adjust: true},
-        {id: 'description', header: '내용', fillspace: true,}
-    ]
-    `
-}
-
-function CONTAINER(id) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://localhost:3000/${id}`);
-    request.onload = function() {
-        const data = JSON(request.response);
-        console.log(data);
-        return data;
-    }
-}
-
-export const getIndex = (req, res) => {
-    const table = TEMP();
-    const body = `
-    rows: [
-        {${table}},
-        {
-            view: 'toolbar', elements: [
-                {
-                    view: 'button', value: '추가',
-                    click: function() {
-                        location.href='/edit';
-                    }
-                }
-            ]
-        }
-    ]
-    `;
-    const template = HTML(body, '');
-    res.send(template);
-}
-
-export const getMovies = (req, res) => {
-    let data = getData();
     res.send(data);
 }
 
@@ -210,16 +34,19 @@ export const getMovie = (req, res) => {
 
     if (id) {
         fs.readFile(`movies/${id}`, 'utf8', (err, movie) => {
-            movie = JSON.parse(movie);
+            if (err) throw err;
+            else {
+                movie = JSON.parse(movie);
 
-            const body = MOVIE_DISPLAY(movie.id, movie.title, movie.director, movie.release_date, movie.description);
+                const body = MOVIE_DISPLAY(movie.id, movie.title, movie.director, movie.release_date, movie.description);
 
-            var template = HTML(body, ''); // no function part
+                var template = HTML(body, ''); // no function part
 
-            res.send(template);
+                res.send(template);
+            }
         });
     } else {
-        webix.message("영화가 존재하지 않습니다.");
+        alert("영화가 존재하지 않습니다.");
         location.href='/';
     }
 }
@@ -259,7 +86,7 @@ export const editMovie = (req, res) => {
     fs.writeFile(`movies/${movie.id}`, `${json}`, 'utf8', (err) => {
         if (err) throw err;
         else {
-            console.log(`${movie.id} has saved!`);
+            // console.log(`${movie.id} has saved!`);
             res.redirect(`/movies/${movie.id}`);
         }
     });
@@ -272,119 +99,22 @@ export const editView = (req, res) => {
     if (id) {
         /* id에 해당하는 영화 데이터 가져와서 각 input의 value 값으로 설정한다. */
         fs.readFile(`movies/${id}`, 'utf8', (err, movie) => {
+            if (err) throw err;
+
             movie = JSON.parse(movie);
 
-            const body = `
-            rows: [
-                {
-                    view: 'form', id: 'myForm', elements: [
-                        {view: 'text', name: 'title', label: '제목', value: '${movie.title}'},
-                        {
-                            cols: [
-                                {view: 'text', name: 'director', label: '감독', value: '${movie.director}'},
-                                {view: 'text', name: 'release_date', label: '개봉년도', value: '${movie.release_date}'}
-                            ]
-                        },
-                        {view: 'textarea', name: 'description', label: '내용', value: '${movie.description}'}
-                    ]
-                },
-                {
-                    view: 'toolbar', elements: [
-                        {view: 'button', value: '완성', click: submitBtn},
-                        {
-                            view: 'button', value: "다시 작성",
-                            click: function() {
-                                $$('myForm').clear();
-                            }
-                        },
-                        {
-                            view: 'button',
-                            value: '뒤로 가기',
-                            click: function() {
-                                history.back();
-                            }
-                        }
-                    ]
-                }
-            ]
-            `;
-        
-            const func = `
-            function submitBtn() {
-                var myForm = $$("myForm");
-                var items = myForm.getValues();
-
-                webix.ajax().post('/edit_process',
-                    {
-                        id: "${movie.id}",
-                        title: items.title,
-                        description: items.description,
-                        director: items.director,
-                        release_date: items.release_date
-                    }).then(() => { location.href='/movies/${movie.id}'; });
-            }
-            `;
+            const body = EDIT(movie.title, movie.director, movie.release_date, movie.description);
+            const func = EDIT_FUNC(movie.id);
             
             const template = HTML(body, func);
             res.send(template);
         });
-    }
-    
-    
-    /* 새 영화 추가 */
-    else {
-        const body = `
-        rows: [
-            {
-                view: 'form', id: 'myForm', elements: [
-                    {view: 'text', name: 'title', label: '제목'},
-                    {
-                        cols: [
-                            {view: 'text', name: 'director', label: '감독'},
-                            {view: 'text', name: 'release_date', label: '개봉년도'}
-                        ]
-                    },
-                    {view: 'textarea', name: 'description', label: '내용'}
-                ]
-            },
-            {
-                view: 'toolbar', elements: [
-                    {view: 'button', value: '완성', click: submitBtn},
-                    {
-                        view: 'button', value: "다시 작성",
-                        click: function() {
-                            $$('myForm').clear();
-                        }
-                    },
-                    {
-                        view: 'button',
-                        value: '뒤로 가기',
-                        click: function() {
-                            history.back();
-                        }
-                    }
-                ]
-            }
-        ]
-        `;
-    
+    } else { /* 새 영화 추가 */
         const created_id = uuidv4(); // 새 영화의 id 생성
-        const func = `
-        function submitBtn() {
-            var myForm = $$("myForm");
-            var items = myForm.getValues();
 
-            webix.ajax().post('/edit_process',
-                {
-                    id: "${created_id}",
-                    title: items.title,
-                    description: items.description,
-                    director: items.director,
-                    release_date: items.release_date
-                }).then(() => { location.href='/movies/${created_id}'; });
-        }
-        `;
-        
+        const body = EDIT();
+        const func = EDIT_FUNC(created_id);
+
         const template = HTML(body, func);
         res.send(template);
     }
